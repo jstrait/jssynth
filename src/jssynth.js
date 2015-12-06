8032c64c91bf2cf51ee41d0bb7e75d4634a8deef
Joel Strait <joel.strait@gmail.com>
2015-12-06 13:53:01 -0800

    Moving code around

--- src/jssynth.js
+++ src/jssynth.js
@@ -2,67 +2,6 @@
 
 var JSSynth = JSSynth || {};
 
-JSSynth.Track = function(instrument, sequence, isMuted) {
-  var track = {};
-
-  track.instrument = instrument; 
-  track.sequence   = sequence;
-  track.isMuted    = isMuted;
-
-  track.setNotes = function(newNotes) {
-    track.sequence = JSSynth.SequenceParser.parse(newNotes);
-  };
-
-  track.reset = function(newCurrentTime) {
-    sequenceIndex = 0;
-    finishedPlaying = false;
-    currentTime = newCurrentTime;
-  };
-
-  track.getIsMuted = function() {
-    return track.isMuted;
-  };
-
-  track.setIsMuted = function(newIsMuted) {
-    track.isMuted = newIsMuted;
-  };
-
-  track.finishedPlaying = function() { return finishPlaying; }
-
-  track.tick = function(endTime, stepDuration, loop) {
-    var note, noteTimeDuration;
-
-    while (currentTime < endTime) {
-      note = track.sequence[sequenceIndex];
-      noteTimeDuration = stepDuration * note.stepDuration;
-      
-      if (!track.isMuted) {
-        track.instrument.playNote(note, currentTime, currentTime + noteTimeDuration);
-      }
-
-      sequenceIndex += 1;
-      if (sequenceIndex >= track.sequence.length) {
-        if (loop) {
-          sequenceIndex = 0;
-        }
-        else {
-          finishedPlaying = true;
-          return;
-        }
-      }
-
-      currentTime += stepDuration;
-    }
-  };
-
-  var sequenceIndex;
-  var currentTime;
-  var finishedPlaying;
-
-  track.reset();
-  return track;
-};
-
 JSSynth.Instrument = function(audioContext, config) {
   var buildOscillator = function(waveform, frequency) {
     var oscillator = audioContext.createOscillator();
@@ -182,173 +121,65 @@ JSSynth.EnvelopeCalculator = {
   },
 };
 
-JSSynth.Transport = function(audioContext, tracks, stopCallback) {
-  var SCHEDULE_AHEAD_TIME = 0.2;  // in seconds
-  var TICK_INTERVAL = 50;         // in milliseconds
+JSSynth.Track = function(instrument, sequence, isMuted) {
+  var track = {};
 
-  var tick = function() {
-    var finalTime = audioContext.currentTime + SCHEDULE_AHEAD_TIME;
+  track.instrument = instrument; 
+  track.sequence   = sequence;
+  track.isMuted    = isMuted;
 
-    tracks.forEach(function(track) {
-      track.tick(finalTime, transport.stepInterval, transport.loop);
-    });
-
-    if (!tracks.some(function(track) { return track.finishedPlaying; })) {
-      stop();
-      window.setTimeout(stopCallback, transport.stepInterval * 1000);
-    }
+  track.setNotes = function(newNotes) {
+    track.sequence = JSSynth.SequenceParser.parse(newNotes);
   };
 
-  var start = function() {
-    tracks.forEach(function(track) {
-      track.reset(audioContext.currentTime);
-    });
-
-    tick();
-    timeoutId = window.setInterval(tick, TICK_INTERVAL);
-    playing = true;
+  track.reset = function(newCurrentTime) {
+    sequenceIndex = 0;
+    finishedPlaying = false;
+    currentTime = newCurrentTime;
   };
 
-  var stop = function() {
-    window.clearInterval(timeoutId);
-    playing = false;
+  track.getIsMuted = function() {
+    return track.isMuted;
   };
 
-  var timeoutId;
-  var playing = false;
-
-
-  var transport = {};
-
-  transport.setTempo = function(newTempo) {
-    transport.tempo = newTempo;
-
-    var sixteenthsPerMinute = transport.tempo * 4;
-    transport.stepInterval = 60.0 / sixteenthsPerMinute;
+  track.setIsMuted = function(newIsMuted) {
+    track.isMuted = newIsMuted;
   };
 
-  transport.toggle = function() {
-    if (playing) {
-      stop();
-    }
-    else {
-      start();
-    }
-  };
+  track.finishedPlaying = function() { return finishPlaying; }
 
-  transport.loop = true;
-  transport.setTempo(100);
+  track.tick = function(endTime, stepDuration, loop) {
+    var note, noteTimeDuration;
 
-  return transport;
-};
-
-
-JSSynth.WaveWriter = function() {
-  var waveWriter = {};
-
-  var littleEndian = true;
-  var audioFormatCode = 1;  // I.e., PCM
-  var numChannels = 1;
-  var bitsPerSample = 16;
-  var bytesPerSample = 2;
-  var sampleRate = 44100;
-
-  var blockAlign = (bitsPerSample / 8) * numChannels;
-  var byteRate = blockAlign * sampleRate;
-
-  var waveFileHeaderByteCount = 44;
-
-  waveWriter.write = function(rawFloat32SampleData) {
-    var sampleDataByteCount = rawFloat32SampleData.length * bytesPerSample;
-    var fileLength = waveFileHeaderByteCount + sampleDataByteCount;
-    var outputView = new DataView(new ArrayBuffer(fileLength));
-
-    outputView.setUint8(  0, "R".charCodeAt(0), littleEndian);
-    outputView.setUint8(  1, "I".charCodeAt(0), littleEndian);
-    outputView.setUint8(  2, "F".charCodeAt(0), littleEndian);
-    outputView.setUint8(  3, "F".charCodeAt(0), littleEndian);
-    outputView.setUint32( 4, 36 + sampleDataByteCount, littleEndian);
-    outputView.setUint8(  8, "W".charCodeAt(0), littleEndian);
-    outputView.setUint8(  9, "A".charCodeAt(0), littleEndian);
-    outputView.setUint8( 10, "V".charCodeAt(0), littleEndian);
-    outputView.setUint8( 11, "E".charCodeAt(0), littleEndian);
-    outputView.setUint8( 12, "f".charCodeAt(0), littleEndian);
-    outputView.setUint8( 13, "m".charCodeAt(0), littleEndian);
-    outputView.setUint8( 14, "t".charCodeAt(0), littleEndian);
-    outputView.setUint8( 15, " ".charCodeAt(0), littleEndian);
-    outputView.setUint32(16, 16, littleEndian);
-    outputView.setUint16(20, audioFormatCode, littleEndian);
-    outputView.setUint16(22, numChannels, littleEndian);
-    outputView.setUint32(24, sampleRate, littleEndian);
-    outputView.setUint32(28, byteRate, littleEndian);
-    outputView.setUint16(32, blockAlign, littleEndian);
-    outputView.setUint16(34, bitsPerSample, littleEndian);
-    outputView.setUint8( 36, "d".charCodeAt(0), littleEndian);
-    outputView.setUint8( 37, "a".charCodeAt(0), littleEndian);
-    outputView.setUint8( 38, "t".charCodeAt(0), littleEndian);
-    outputView.setUint8( 39, "a".charCodeAt(0), littleEndian);
-    outputView.setUint32(40, sampleDataByteCount, littleEndian);
-
-    rawFloat32SampleData.forEach(function(sample, index) {
-      // Should this round?
-      outputView.setInt16(waveFileHeaderByteCount + (index * bytesPerSample), sample * 32767.0, littleEndian);
-    });
-
-    return outputView;
-  };
-
-  return waveWriter;
-};
-
-JSSynth.OfflineTransport = function(offlineAudioContext, tracks, filename, completeCallback) {
-  var transport = {};
-
-  offlineAudioContext.oncomplete = function(e) {
-    var waveWriter = new JSSynth.WaveWriter();
-    var outputView = waveWriter.write(e.renderedBuffer.getChannelData(0));
-
-    var blob = new Blob([outputView], { type: "audio/wav" });
-    var url  = window.URL.createObjectURL(blob);
-    document.getElementById("downloaded-file").src = url;
-
-    var hiddenDownloadLink = document.getElementById("hidden-download-link");
-    hiddenDownloadLink.download = filename + ".wav";
-    hiddenDownloadLink.href = url;
-    hiddenDownloadLink.click();
-
-    window.URL.revokeObjectURL(blob);
-  };
-
-  transport.tick = function() {
-    var nextNoteTime, noteTimeDuration;
-    var startTime = offlineAudioContext.currentTime;
-
-    tracks.forEach(function(track) {
-      if (!track.getIsMuted()) {
-        nextNoteTime = startTime;
-
-        track.sequence.forEach(function(note) {
-          noteTimeDuration = transport.stepInterval * note.stepDuration;
-          track.instrument.playNote(note, nextNoteTime, nextNoteTime + noteTimeDuration);
-
-          nextNoteTime += noteTimeDuration;
-        });
+    while (currentTime < endTime) {
+      note = track.sequence[sequenceIndex];
+      noteTimeDuration = stepDuration * note.stepDuration;
+      
+      if (!track.isMuted) {
+        track.instrument.playNote(note, currentTime, currentTime + noteTimeDuration);
       }
-    });
 
-    offlineAudioContext.startRendering();
+      sequenceIndex += 1;
+      if (sequenceIndex >= track.sequence.length) {
+        if (loop) {
+          sequenceIndex = 0;
+        }
+        else {
+          finishedPlaying = true;
+          return;
+        }
+      }
+
+      currentTime += stepDuration;
+    }
   };
 
-  transport.setTempo = function(newTempo) {
-    transport.tempo = newTempo;
+  var sequenceIndex;
+  var currentTime;
+  var finishedPlaying;
 
-    var sixteenthsPerMinute = transport.tempo * 4;
-    transport.stepInterval = 60.0 / sixteenthsPerMinute;
-  };
-
-  transport.setTempo(100);
-
-  return transport;
+  track.reset();
+  return track;
 };
 
 
@@ -473,4 +304,172 @@ JSSynth.MusicTheory = {
 
   MIDDLE_OCTAVE: 4,
   MIDDLE_A_FREQUENCY: 440.0,
+};
+
+JSSynth.WaveWriter = function() {
+  var waveWriter = {};
+
+  var littleEndian = true;
+  var audioFormatCode = 1;  // I.e., PCM
+  var numChannels = 1;
+  var bitsPerSample = 16;
+  var bytesPerSample = 2;
+  var sampleRate = 44100;
+
+  var blockAlign = (bitsPerSample / 8) * numChannels;
+  var byteRate = blockAlign * sampleRate;
+
+  var waveFileHeaderByteCount = 44;
+
+  waveWriter.write = function(rawFloat32SampleData) {
+    var sampleDataByteCount = rawFloat32SampleData.length * bytesPerSample;
+    var fileLength = waveFileHeaderByteCount + sampleDataByteCount;
+    var outputView = new DataView(new ArrayBuffer(fileLength));
+
+    outputView.setUint8(  0, "R".charCodeAt(0), littleEndian);
+    outputView.setUint8(  1, "I".charCodeAt(0), littleEndian);
+    outputView.setUint8(  2, "F".charCodeAt(0), littleEndian);
+    outputView.setUint8(  3, "F".charCodeAt(0), littleEndian);
+    outputView.setUint32( 4, 36 + sampleDataByteCount, littleEndian);
+    outputView.setUint8(  8, "W".charCodeAt(0), littleEndian);
+    outputView.setUint8(  9, "A".charCodeAt(0), littleEndian);
+    outputView.setUint8( 10, "V".charCodeAt(0), littleEndian);
+    outputView.setUint8( 11, "E".charCodeAt(0), littleEndian);
+    outputView.setUint8( 12, "f".charCodeAt(0), littleEndian);
+    outputView.setUint8( 13, "m".charCodeAt(0), littleEndian);
+    outputView.setUint8( 14, "t".charCodeAt(0), littleEndian);
+    outputView.setUint8( 15, " ".charCodeAt(0), littleEndian);
+    outputView.setUint32(16, 16, littleEndian);
+    outputView.setUint16(20, audioFormatCode, littleEndian);
+    outputView.setUint16(22, numChannels, littleEndian);
+    outputView.setUint32(24, sampleRate, littleEndian);
+    outputView.setUint32(28, byteRate, littleEndian);
+    outputView.setUint16(32, blockAlign, littleEndian);
+    outputView.setUint16(34, bitsPerSample, littleEndian);
+    outputView.setUint8( 36, "d".charCodeAt(0), littleEndian);
+    outputView.setUint8( 37, "a".charCodeAt(0), littleEndian);
+    outputView.setUint8( 38, "t".charCodeAt(0), littleEndian);
+    outputView.setUint8( 39, "a".charCodeAt(0), littleEndian);
+    outputView.setUint32(40, sampleDataByteCount, littleEndian);
+
+    rawFloat32SampleData.forEach(function(sample, index) {
+      // Should this round?
+      outputView.setInt16(waveFileHeaderByteCount + (index * bytesPerSample), sample * 32767.0, littleEndian);
+    });
+
+    return outputView;
+  };
+
+  return waveWriter;
+};
+
+JSSynth.Transport = function(audioContext, tracks, stopCallback) {
+  var SCHEDULE_AHEAD_TIME = 0.2;  // in seconds
+  var TICK_INTERVAL = 50;         // in milliseconds
+
+  var tick = function() {
+    var finalTime = audioContext.currentTime + SCHEDULE_AHEAD_TIME;
+
+    tracks.forEach(function(track) {
+      track.tick(finalTime, transport.stepInterval, transport.loop);
+    });
+
+    if (!tracks.some(function(track) { return track.finishedPlaying; })) {
+      stop();
+      window.setTimeout(stopCallback, transport.stepInterval * 1000);
+    }
+  };
+
+  var start = function() {
+    tracks.forEach(function(track) {
+      track.reset(audioContext.currentTime);
+    });
+
+    tick();
+    timeoutId = window.setInterval(tick, TICK_INTERVAL);
+    playing = true;
+  };
+
+  var stop = function() {
+    window.clearInterval(timeoutId);
+    playing = false;
+  };
+
+  var timeoutId;
+  var playing = false;
+
+
+  var transport = {};
+
+  transport.setTempo = function(newTempo) {
+    transport.tempo = newTempo;
+
+    var sixteenthsPerMinute = transport.tempo * 4;
+    transport.stepInterval = 60.0 / sixteenthsPerMinute;
+  };
+
+  transport.toggle = function() {
+    if (playing) {
+      stop();
+    }
+    else {
+      start();
+    }
+  };
+
+  transport.loop = true;
+  transport.setTempo(100);
+
+  return transport;
+};
+
+JSSynth.OfflineTransport = function(offlineAudioContext, tracks, filename, completeCallback) {
+  var transport = {};
+
+  offlineAudioContext.oncomplete = function(e) {
+    var waveWriter = new JSSynth.WaveWriter();
+    var outputView = waveWriter.write(e.renderedBuffer.getChannelData(0));
+
+    var blob = new Blob([outputView], { type: "audio/wav" });
+    var url  = window.URL.createObjectURL(blob);
+    document.getElementById("downloaded-file").src = url;
+
+    var hiddenDownloadLink = document.getElementById("hidden-download-link");
+    hiddenDownloadLink.download = filename + ".wav";
+    hiddenDownloadLink.href = url;
+    hiddenDownloadLink.click();
+
+    window.URL.revokeObjectURL(blob);
+  };
+
+  transport.tick = function() {
+    var nextNoteTime, noteTimeDuration;
+    var startTime = offlineAudioContext.currentTime;
+
+    tracks.forEach(function(track) {
+      if (!track.getIsMuted()) {
+        nextNoteTime = startTime;
+
+        track.sequence.forEach(function(note) {
+          noteTimeDuration = transport.stepInterval * note.stepDuration;
+          track.instrument.playNote(note, nextNoteTime, nextNoteTime + noteTimeDuration);
+
+          nextNoteTime += noteTimeDuration;
+        });
+      }
+    });
+
+    offlineAudioContext.startRendering();
+  };
+
+  transport.setTempo = function(newTempo) {
+    transport.tempo = newTempo;
+
+    var sixteenthsPerMinute = transport.tempo * 4;
+    transport.stepInterval = 60.0 / sixteenthsPerMinute;
+  };
+
+  transport.setTempo(100);
+
+  return transport;
 };
