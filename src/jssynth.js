009a513f57b1f6ccfb6744186afbc3692f1ebd6b
Joel Strait <joel.strait@gmail.com>
2016-06-05 10:27:52 -0700

    Inlining constants
    They are only used by this function, so don't see a reason
    to make them visible in a broader scope

--- src/jssynth.js
+++ src/jssynth.js
@@ -191,10 +191,77 @@ JSSynth.SequenceParser = {
 };
 
 JSSynth.Note = function(newNoteName, newOctave, newStepDuration) {
+  var NOTE_RATIOS = {
+    "A"  : 1.0,
+    "A#" : 16.0 / 15.0,
+    "B"  : 9.0 / 8.0,
+    "C"  : 6.0 / 5.0,
+    "C#" : 5.0 / 4.0,
+    "D"  : 4.0 / 3.0,
+    "D#" : 45.0 / 32.0,
+    "E"  : 3.0 / 2.0,
+    "F"  : 8.0 / 5.0,
+    "F#" : 5.0 / 3.0,
+    "G"  : 9.0 / 5.0,
+    "G#" : 15.0 / 8.0,
+  };
+
+  var ENHARMONIC_EQUIVALENTS = {
+    "A"   : "A",
+    "G##" : "A",
+    "Bbb" : "A",
+
+    "A#"  : "A#",
+    "Bb"  : "A#",
+    "Cbb" : "A#",
+
+    "B"   : "B",
+    "A##" : "B",
+    "Cb"  : "B",
+
+    "C"   : "C",
+    "B#"  : "C",
+    "Dbb" : "C",
+
+    "C#"  : "C#",
+    "B##" : "C#",
+    "Db"  : "C#",
+
+    "D"   : "D",
+    "C##" : "D",
+    "Ebb" : "D",
+
+    "D#"  : "D#",
+    "Eb"  : "D#",
+    "Fbb" : "D#",
+
+    "E"   : "E",
+    "D##" : "E",
+    "Fb"  : "E",
+
+    "F"   : "F",
+    "E#"  : "F",
+    "Gbb" : "F",
+
+    "F#"  : "F#",
+    "E##" : "F#",
+    "Gb"  : "F#",
+
+    "G"   : "G",
+    "F##" : "G",
+    "Abb" : "G",
+
+    "G#"  : "G#",
+    "Ab"  : "G#",
+  };
+
+  var MIDDLE_OCTAVE = 4;
+  var MIDDLE_A_FREQUENCY = 440.0;
+
   var calculateFrequency = function(noteName, octave) {
-    noteName = JSSynth.MusicTheory.ENHARMONIC_EQUIVALENTS[noteName];
-    var octaveMultiplier = Math.pow(2.0, (octave - JSSynth.MusicTheory.MIDDLE_OCTAVE));
-    var frequency = JSSynth.MusicTheory.NOTE_RATIOS[noteName] * JSSynth.MusicTheory.MIDDLE_A_FREQUENCY * octaveMultiplier;
+    noteName = ENHARMONIC_EQUIVALENTS[noteName];
+    var octaveMultiplier = Math.pow(2.0, (octave - MIDDLE_OCTAVE));
+    var frequency = NOTE_RATIOS[noteName] * MIDDLE_A_FREQUENCY * octaveMultiplier;
 
     return frequency;
   };
@@ -212,75 +279,6 @@ JSSynth.Note = function(newNoteName, newOctave, newStepDuration) {
   note.frequency    = function() { return frequency; };
 
   return note;
-};
-
-JSSynth.MusicTheory = {
-  NOTE_RATIOS: {
-    "A"  : 1.0,
-    "A#" : 16.0 / 15.0,
-    "B"  : 9.0 / 8.0,
-    "C"  : 6.0 / 5.0,
-    "C#" : 5.0 / 4.0,
-    "D"  : 4.0 / 3.0,
-    "D#" : 45.0 / 32.0,
-    "E"  : 3.0 / 2.0,
-    "F"  : 8.0 / 5.0,
-    "F#" : 5.0 / 3.0,
-    "G"  : 9.0 / 5.0,
-    "G#" : 15.0 / 8.0
-  },
-
-  ENHARMONIC_EQUIVALENTS: {
-    "A"   : "A",
-    "G##" : "A",
-    "Bbb" : "A",
-    
-    "A#"  : "A#",
-    "Bb"  : "A#",
-    "Cbb" : "A#",
-    
-    "B"   : "B",
-    "A##" : "B",
-    "Cb"  : "B",
-    
-    "C"   : "C",
-    "B#"  : "C",
-    "Dbb" : "C",
-    
-    "C#"  : "C#",
-    "B##" : "C#",
-    "Db"  : "C#",
-    
-    "D"   : "D",
-    "C##" : "D",
-    "Ebb" : "D",
-    
-    "D#"  : "D#",
-    "Eb"  : "D#",
-    "Fbb" : "D#",
-    
-    "E"   : "E",
-    "D##" : "E",
-    "Fb"  : "E",
-    
-    "F"   : "F",
-    "E#"  : "F",
-    "Gbb" : "F",
-    
-    "F#"  : "F#",
-    "E##" : "F#",
-    "Gb"  : "F#",
-    
-    "G"   : "G",
-    "F##" : "G",
-    "Abb" : "G",
-    
-    "G#"  : "G#",
-    "Ab"  : "G#"
-  },
-
-  MIDDLE_OCTAVE: 4,
-  MIDDLE_A_FREQUENCY: 440.0,
 };
 
 
