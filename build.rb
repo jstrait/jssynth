964cb6fd176c785963147869654f0577b337a71c
Joel Strait <joel.strait@gmail.com>
2016-11-16 19:06:47 -0800

    Compressing CSS during build process
    Using UglifyCSS

--- build.rb
+++ build.rb
@@ -4,7 +4,7 @@ require 'rb-fsevent'
 
 def build
   `uglifyjs src/*.js --compress --mangle --source-map jssynth.js.map > jssynth.js`
-  `sass css/jssynth.scss jssynth.css`
+  `sass css/jssynth.scss | uglifycss > jssynth.css`
 end
 
 option = (ARGV[0] || '').downcase
