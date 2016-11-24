ba94a46d812f74043877a1c78cbeef5d3f4bbf35
Joel Strait <joel.strait@gmail.com>
2016-11-23 19:00:33 -0800

    Renaming "/css" folder to "/sass"
    Since the folder contains Sass files, not CSS files

--- build.rb
+++ build.rb
@@ -4,7 +4,7 @@ require 'rb-fsevent'
 
 def build
   `uglifyjs src/*.js src/**/*.js --compress --mangle --source-map jssynth.js.map > jssynth.js`
-  `sass css/jssynth.scss | uglifycss > jssynth.css`
+  `sass sass/jssynth.scss | uglifycss > jssynth.css`
 end
 
 option = (ARGV[0] || '').downcase
@@ -12,7 +12,7 @@ option = (ARGV[0] || '').downcase
 if option == '-w' || option == '--watch'
   fsevent = FSEvent.new
 
-  fsevent.watch ['src', 'css'] do |directories|
+  fsevent.watch ['src', 'sass'] do |directories|
     puts "Detected source file change, rebuilding"
     build
   end
