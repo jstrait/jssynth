jstrait/jssynth

commit f53e72bd48b244abf64780c1a94f9776004aaae7

Preventing pops and clicks during playback
It appears to be caused by suddenly setting the gain
volume to 0 at the same time as note playback starts.

To fix this, the gain volume is set to 0 at time 0,
so that it is guaranteed to already be at 0 before
the note starts playing.

This works because a new gain node is constructed
each time a note is played, so it doesn't matter
_when_ the gain volume was set to 0, as long as it
happens before the note starts playing. By setting
the volume to 0 at time 0, then this will occur
for any note (except for notes that start at time
0, but those don't seem to have a problem with
pops/clicks).

Also, popping/clicking seemed to be browser
dependent - noticeable in Safari and Firebox,
less so in Chrome.

src/jssynth.js
@@ -75,7 +75,7 @@ JSSynth.Instrument = function(config) {
       var calculatedEnvelope = JSSynth.EnvelopeCalculator.calculate(amplitude, config.envelope, gateOnTime, gateOffTime);
 
       // Envelope Attack
-      masterGain.gain.setValueAtTime(0.0, gateOnTime);
+      masterGain.gain.setValueAtTime(0.0, 0.0);
       masterGain.gain.linearRampToValueAtTime(calculatedEnvelope.attackEndAmplitude, calculatedEnvelope.attackEndTime);
 
       // Envelope Decay/Sustain

commit 2138be314f96e5e71fce6f8bc55b8675919b3df1

Fixing volume envelope attack so it actually works
Fixes bug introduced in f53e72bd48b244abf64780c1a94f9776004aaae7

The bug was caused by the amplitude attack starting from time
0 in the song, instead of the note's gate on time. This made
the ramp really long (and longer and longer as the song played),
meaning that when the note began playing the ramp was almost
complete. This had the effect of essentially making the attack
parameter non-functional.

So far I haven't heard the pops and clicks during playback
mentioned in the previous commit in Safari, Firefox or Chrome
on the laptop I'm testing this on. After testing on other
devices might need to tweak the attack ramp start time
(current at 0.001 for the reasons mentioned in
f53e72bd48b244abf64780c1a94f9776004aaae7).

src/jssynth.js
@@ -74,7 +74,7 @@ JSSynth.Instrument = function(config) {
     var calculatedEnvelope = JSSynth.EnvelopeCalculator.calculate(amplitude, config.envelope, gateOnTime, gateOffTime);
 
     // Envelope Attack
-    masterGain.gain.setValueAtTime(0.0, 0.0);
+    masterGain.gain.setValueAtTime(0.0, gateOnTime - 0.001);
     masterGain.gain.linearRampToValueAtTime(calculatedEnvelope.attackEndAmplitude, calculatedEnvelope.attackEndTime);
 
     // Envelope Decay/Sustain