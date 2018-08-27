jstrait/jssynth

commit 1189db55ce4ea10a936a4f603df49c85d8b0c966

Left/right arrow keys move to next note, regardless of cursor position
Since the text boxes are not normal freely editable text boxes anymore,
it makes less sense to allow moving the cursor within the text box
instead of jumping to the next note.

src/components/pattern_editor.js
@@ -130,12 +130,12 @@ class NoteInput extends React.Component {
       this.props.setNoteValue("", this.props.patternID, this.props.rowIndex, this.props.noteIndex);
     }
     else if (e.keyCode === LEFT_ARROW) {
-      if (element.selectionStart === 0 && !(element.classList.contains('firstNote'))) {
+      if (!(element.classList.contains('firstNote'))) {
         this.changeCurrentlySelectedNote(0, -1);
       }
     }
     else if (e.keyCode === RIGHT_ARROW) {
-      if (element.selectionEnd === element.value.length && !(element.classList.contains('lastNote'))) {
+      if (!(element.classList.contains('lastNote'))) {
         this.changeCurrentlySelectedNote(0, 1);
       }
     }

commit c8692fb7d9c38dbd76e6c85f41b9c882933a3b77

Fixing note inputs to allow "-" character to be entered
Inadvertently broken in 1189db55ce4ea10a936a4f603df49c85d8b0c966

src/components/pattern_editor.js
@@ -135,7 +135,7 @@ class NoteInput extends React.Component {
       this.props.setNoteValue(this.unformatNote(String.fromCharCode(e.keyCode) + noteParts.modifier + noteParts.octave), this.props.patternID, this.props.rowIndex, this.props.noteIndex);
     }
     else if (e.keyCode === DASH && !e.shiftKey) {
-      this.props.setNoteValue("", this.props.patternID, this.props.rowIndex, this.props.noteIndex);
+      this.props.setNoteValue("-", this.props.patternID, this.props.rowIndex, this.props.noteIndex);
     }
     else if (e.keyCode === LEFT_ARROW) {
       if (this.props.noteIndex > 0) {