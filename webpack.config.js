32fd8bd0471e6880a09d8d680eba2ff5968bf6c6
Joel Strait <joel.strait@gmail.com>
2018-10-18 16:37:00 -0700

    Adding semicolons for code style reasons

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: 'production',
