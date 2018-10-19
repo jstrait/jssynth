c4000ea219aff7fc327d338d3d146c9f0c9f1939
Joel Strait <joel.strait@gmail.com>
2018-10-18 17:44:15 -0700

    Building CSS via Webpack
    Unifies the build process into one pipeline, and allows live rebuilding
    if a CSS file changes via the Webpack --watch option.
    
    As a result, remove the UglifyCSS dependency (not used by the Webpack
    build process), and the dedicated `build-css` script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/app.js',
+  entry: ['./src/app.js', './sass/jssynth.scss'],
   module: {
     rules: [
       {
@@ -16,16 +18,42 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ]
       }
     ]
   },
-  plugins: [
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+ plugins: [
     new CopyWebpackPlugin([
       { from: 'jssynth.html', to: 'build/' },
       { from: 'sounds/*.wav', to: 'build/' },
       { from: 'images/*.png', to: 'build/' },
       { from: 'lib/*.js', to: 'build/' },
     ], {}),
+    new MiniCssExtractPlugin({
+        filename: "build/jssynth.css",
+    }),
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: true,
