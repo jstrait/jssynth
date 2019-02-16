302903f53d127bcb95f9cbf49256043633946570
Joel Strait <joel.strait@gmail.com>
2019-02-16 11:15:54 -0800

    More succinct/DRY module exports
    I didn't realize this syntax existed when first implementing
    these exports. The benefit is that each export now only needs
    to be referred to once, instead of twice.

--- src/synth_core.js
+++ src/synth_core.js
@@ -1,35 +1,17 @@
 "use strict";
 
-import { AudioContextBuilder } from "./synth_core/audio_context_builder";
-import { AudioSource } from "./synth_core/audio_source";
-import { BufferCollection } from "./synth_core/buffer_collection";
-import { Envelope } from "./synth_core/instrument";
-import { InstrumentNote } from "./synth_core/instrument_note";
-import { OfflineTransport } from "./synth_core/offline_transport";
-import { Note } from "./synth_core/note";
-import { NotePlayer } from "./synth_core/note_player";
-import { SampleInstrument } from "./synth_core/instrument";
-import { Score } from "./synth_core/score";
-import { SequenceParser } from "./synth_core/sequence_parser";
-import { SongPlayer } from "./synth_core/song_player";
-import { SynthInstrument } from "./synth_core/instrument";
-import { Transport } from "./synth_core/transport";
-import { WaveWriter } from "./synth_core/wave_writer";
-
-export {
-  AudioContextBuilder,
-  AudioSource,
-  BufferCollection,
-  Envelope,
-  InstrumentNote,
-  OfflineTransport,
-  Note,
-  NotePlayer,
-  SampleInstrument,
-  Score,
-  SequenceParser,
-  SongPlayer,
-  SynthInstrument,
-  Transport,
-  WaveWriter,
-};
+export { AudioContextBuilder } from "./synth_core/audio_context_builder";
+export { AudioSource } from "./synth_core/audio_source";
+export { BufferCollection } from "./synth_core/buffer_collection";
+export { Envelope } from "./synth_core/instrument";
+export { InstrumentNote } from "./synth_core/instrument_note";
+export { OfflineTransport } from "./synth_core/offline_transport";
+export { Note } from "./synth_core/note";
+export { NotePlayer } from "./synth_core/note_player";
+export { SampleInstrument } from "./synth_core/instrument";
+export { Score } from "./synth_core/score";
+export { SequenceParser } from "./synth_core/sequence_parser";
+export { SongPlayer } from "./synth_core/song_player";
+export { SynthInstrument } from "./synth_core/instrument";
+export { Transport } from "./synth_core/transport";
+export { WaveWriter } from "./synth_core/wave_writer";
