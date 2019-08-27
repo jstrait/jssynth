jstrait/jssynth

commit 2c854994ab63a05e756eeb75254e3412d79d9e0a

More robust method of clamping pattern to bounds while dragging
The clamping now occurs in the UI layer, by clamping the raw pixel
coordinate of the mouse/touch input, rather than at the service
layer by clamping the step index/track index.

This seems a little better because it seems a little strange for
the service layer to clamp to step index/track index rather than
throwing an error.

This is a step toward implementing the ability to drag patterns using
touch - the relevant clamping isn't fully necessary when using the
mouse, because mouse events will be clamped to the container anyway,
unlike touch events.

src/app.js
@@ -720,8 +720,6 @@ class App extends React.Component {
     let otherPatternEndStep;
     let i;
 
-    newStartStep = Math.max(0, newStartStep);
-    newStartStep = Math.min((this.state.measureCount * 16) - pattern.rows[0].notes.length, newStartStep);
     newEndStep = newStartStep + pattern.rows[0].notes.length;
 
     if (pattern.trackID === newTrackID && pattern.startStep === newStartStep) {

src/components/sequencer.js
@@ -88,6 +88,8 @@ class TimelineGrid extends React.Component {
   constructor(props) {
     super(props);
 
+    this.timelineWidthInPixels = this.props.measureCount * STEPS_PER_MEASURE * STEP_WIDTH_IN_PIXELS;
+
     this.state = {
       isDragInProgress: false,
     };
@@ -128,6 +130,17 @@ class TimelineGrid extends React.Component {
     let xOffset = e.clientX - containerBoundingRect.left - 16;
     let yOffset = e.clientY - containerBoundingRect.top;
 
+    // We can't use `this.containerEl.width` to check the bounds, because it
+    // will have a different value depending on how wide the window is, because
+    // the container automatically expands to fill the available space.
+    xOffset = Math.max(0, xOffset);
+    xOffset = Math.min(xOffset, this.timelineWidthInPixels - 1);
+
+    // Unlike xOffset, the container always has the same height regardless of the
+    // height of the window, so we can use it for clamping.
+    yOffset = Math.max(0, yOffset);
+    yOffset = Math.min(yOffset, containerBoundingRect.height - 1);
+
     let stepUnderCursor = Math.floor((xOffset / STEP_WIDTH_IN_PIXELS));
     let newStartStep = Math.floor(stepUnderCursor / STEPS_PER_MEASURE) * STEPS_PER_MEASURE;
 
@@ -158,7 +171,7 @@ class TimelineGrid extends React.Component {
       {this.props.tracks.map((track, trackIndex) =>
       <li key={trackIndex} className="list-style-none flex full-width height-3">
         <span className="sequencer-row-left-padding border-box bb br bg-lighter-gray"></span>
-        <span className="sequencer-row border-box bb br" style={{minWidth: (this.props.measureCount * STEPS_PER_MEASURE * STEP_WIDTH_IN_PIXELS) + "px"}}>
+        <span className="sequencer-row border-box bb br" style={{minWidth: this.timelineWidthInPixels + "px"}}>
           {this.props.patternsByTrackID[track.id].map((pattern, patternIndex) =>
           <TimelinePattern key={patternIndex}
                            patternID={pattern.id}

commit 7efbd00c7c3c7227fbd313a8875a8466e67a0fee

Preventing error when dragging multi-measure pattern past end of song
2c854994ab63a05e756eeb75254e3412d79d9e0a accidentally introduced this
bug, due to removing the clamping of the pattern start step so that the
pattern would never extend past the end of the song.

src/app.js
@@ -720,6 +720,7 @@ class App extends React.Component {
     let otherPatternEndStep;
     let i;
 
+    newStartStep = Math.min((this.state.measureCount * 16) - pattern.rows[0].notes.length, newStartStep);
     newEndStep = newStartStep + pattern.rows[0].notes.length;
 
     if (pattern.trackID === newTrackID && pattern.startStep === newStartStep) {