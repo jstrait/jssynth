74b346ed8de0a4e2834312ed38006e669d472262
Joel Strait <joel.strait@gmail.com>
2019-12-05 22:10:18 -0800

    Slightly DRYing sequencer pattern box markup
    Since the "loop" and "resize" pattern box buttons have the same
    width, it's a little simpler to instead set the width once on the
    parent container.

--- src/components/sequencer.js
+++ src/components/sequencer.js
@@ -438,9 +438,9 @@ class TimelinePattern extends React.Component {
               style={{left: ((this.props.baseStepCount * STEP_WIDTH_IN_PIXELS * index) - ((index === 0) ? 0 : 1)) + "px",
                       width: ((SUB_PATTERN_LENGTHS[index] * STEP_WIDTH_IN_PIXELS) - 1 + ((index > 0) ? 1 : 0) + ((index < (SUB_PATTERN_LENGTHS.length - 1)) ? 1 : 0)) + "px"}}>
           {index === (SUB_PATTERN_LENGTHS.length - 1) &&
-          <span className="flex flex-column full-height right bg-gray">
-            <span className="flex-uniform-size width-1 h4 center" onMouseDown={this.onStartLoopChange} onTouchStart={this.onStartLoopChange}>&#8635;</span>
-            {this.props.baseStepCount === this.props.fullStepCount && <span className="flex-uniform-size width-1 h4 center" onMouseDown={this.onStartResize} onTouchStart={this.onStartResize}>&harr;</span>}
+          <span className="flex flex-column width-1 full-height right bg-gray">
+            <span className="flex-uniform-size h4 center" onMouseDown={this.onStartLoopChange} onTouchStart={this.onStartLoopChange}>&#8635;</span>
+            {this.props.baseStepCount === this.props.fullStepCount && <span className="flex-uniform-size h4 center" onMouseDown={this.onStartResize} onTouchStart={this.onStartResize}>&harr;</span>}
           </span>
           }
         </span>
