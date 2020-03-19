584e77c56c0e54039d67e93334e3ea833ac03784
Joel Strait <joel.strait@gmail.com>
2020-03-19 16:42:33 -0700

    Adding more detailed tests for sequence parsing
    This is being done to better document the existing behavior in
    advance of making some implementation changes to sequence parsing.

--- spec/synth_core.test.js
+++ spec/synth_core.test.js
@@ -67,6 +67,9 @@ describe("SynthCore.SequenceParser", function() {
     var rawSequence = "A4 B@2  C#5 ";
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
 
+    expect(parsedSequence.length).toEqual(5);
+    expect(Object.keys(parsedSequence)).toEqual(["0", "1", "2", "3", "4"]);
+
     expect(parsedSequence[0].name()).toEqual("A");
     expect(parsedSequence[0].octave()).toEqual(4);
     expect(parsedSequence[0].stepCount()).toEqual(1);
@@ -75,9 +78,17 @@ describe("SynthCore.SequenceParser", function() {
     expect(parsedSequence[1].octave()).toEqual(2);
     expect(parsedSequence[1].stepCount()).toEqual(1);
 
+    expect(parsedSequence[2].name()).toEqual("");
+    expect(parsedSequence[2].octave()).toBe(NaN);
+    expect(parsedSequence[2].stepCount()).toEqual(1);
+
     expect(parsedSequence[3].name()).toEqual("C#");
     expect(parsedSequence[3].octave()).toEqual(5);
     expect(parsedSequence[3].stepCount()).toEqual(1);
+
+    expect(parsedSequence[4].name()).toEqual("");
+    expect(parsedSequence[4].octave()).toBe(NaN);
+    expect(parsedSequence[4].stepCount()).toEqual(1);
   });
 
   it("should properly parse a sequence containing ties", function() {
@@ -85,13 +96,22 @@ describe("SynthCore.SequenceParser", function() {
 
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
 
+    expect(parsedSequence.length).toEqual(8);
+    expect(Object.keys(parsedSequence)).toEqual(["0", "4", "6", "7"]);
+
     expect(parsedSequence[0].name()).toEqual("A");
     expect(parsedSequence[0].octave()).toEqual(4);
     expect(parsedSequence[0].stepCount()).toEqual(4);
 
+    expect(parsedSequence[1]).toBe(undefined);
+    expect(parsedSequence[2]).toBe(undefined);
+    expect(parsedSequence[3]).toBe(undefined);
+
     expect(parsedSequence[4].name()).toEqual("C");
     expect(parsedSequence[4].octave()).toEqual(2);
     expect(parsedSequence[4].stepCount()).toEqual(2);
+
+    expect(parsedSequence[5]).toBe(undefined);
 
     expect(parsedSequence[6].name()).toEqual("D");
     expect(parsedSequence[6].octave()).toEqual(4);
@@ -103,13 +123,36 @@ describe("SynthCore.SequenceParser", function() {
   });
 
   it("should properly parse a sequence with bad note names", function() {
-    var rawSequence = "V3 - - -";
+    var rawSequence = "V3 - - - 4 A @5 3A";
 
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
+
+    expect(parsedSequence.length).toEqual(8);
+    expect(Object.keys(parsedSequence)).toEqual(["0", "4", "5", "6", "7"]);
 
     expect(parsedSequence[0].name()).toEqual("V");
     expect(parsedSequence[0].octave()).toEqual(3);
     expect(parsedSequence[0].stepCount()).toEqual(4);
+
+    expect(parsedSequence[1]).toBe(undefined);
+    expect(parsedSequence[2]).toBe(undefined);
+    expect(parsedSequence[3]).toBe(undefined);
+
+    expect(parsedSequence[4].name()).toEqual("");
+    expect(parsedSequence[4].octave()).toEqual(4);
+    expect(parsedSequence[4].stepCount()).toEqual(1);
+
+    expect(parsedSequence[5].name()).toEqual("");
+    expect(parsedSequence[5].octave()).toBe(NaN);
+    expect(parsedSequence[5].stepCount()).toEqual(1);
+
+    expect(parsedSequence[6].name()).toEqual("@");
+    expect(parsedSequence[6].octave()).toBe(5);
+    expect(parsedSequence[6].stepCount()).toEqual(1);
+
+    expect(parsedSequence[7].name()).toEqual("3");
+    expect(parsedSequence[7].octave()).toBe(NaN);
+    expect(parsedSequence[7].stepCount()).toEqual(1);
   });
 
   it("should properly parse a sequence containing trailing spaces", function() {
@@ -117,9 +160,28 @@ describe("SynthCore.SequenceParser", function() {
 
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
 
+    expect(parsedSequence.length).toBe(7);
+    expect(Object.keys(parsedSequence)).toEqual(["0", "4", "5", "6"]);
+
     expect(parsedSequence[0].name()).toEqual("A");
     expect(parsedSequence[0].octave()).toEqual(4);
     expect(parsedSequence[0].stepCount()).toEqual(4);
+
+    expect(parsedSequence[1]).toBe(undefined);
+    expect(parsedSequence[2]).toBe(undefined);
+    expect(parsedSequence[3]).toBe(undefined);
+
+    expect(parsedSequence[4].name()).toEqual("");
+    expect(parsedSequence[4].octave()).toBe(NaN);
+    expect(parsedSequence[4].stepCount()).toEqual(1);
+
+    expect(parsedSequence[5].name()).toEqual("");
+    expect(parsedSequence[5].octave()).toBe(NaN);
+    expect(parsedSequence[5].stepCount()).toEqual(1);
+
+    expect(parsedSequence[6].name()).toEqual("");
+    expect(parsedSequence[6].octave()).toBe(NaN);
+    expect(parsedSequence[6].stepCount()).toEqual(1);
   });
 
   it("should properly parse a sequence with unattached sustain characters ('-')", function() {
@@ -127,9 +189,21 @@ describe("SynthCore.SequenceParser", function() {
 
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
 
+    expect(parsedSequence.length).toEqual(6);
+    expect(Object.keys(parsedSequence)).toEqual(["0", "2", "5"]);
+
     expect(parsedSequence[0].name()).toEqual("A");
     expect(parsedSequence[0].octave()).toEqual(4);
     expect(parsedSequence[0].stepCount()).toEqual(2);
+
+    expect(parsedSequence[1]).toBe(undefined);
+
+    expect(parsedSequence[2].name()).toEqual("");
+    expect(parsedSequence[2].octave()).toBe(NaN);
+    expect(parsedSequence[2].stepCount()).toEqual(3);
+
+    expect(parsedSequence[3]).toBe(undefined);
+    expect(parsedSequence[4]).toBe(undefined);
 
     expect(parsedSequence[5].name()).toEqual("C");
     expect(parsedSequence[5].octave()).toEqual(2);
@@ -142,6 +216,8 @@ describe("SynthCore.SequenceParser", function() {
     var parsedSequence = new SynthCore.SequenceParser.parse(rawSequence);
 
     expect(parsedSequence).toEqual([]);
+    expect(parsedSequence.length).toEqual(0);
+    expect(Object.keys(parsedSequence)).toEqual([]);
   });
 });
 
