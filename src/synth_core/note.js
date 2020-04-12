af4ca0176533c357fd45b836e924db8196bc79dd
Joel Strait <joel.strait@gmail.com>
2020-04-11 23:35:07 -0700

    Avoiding unnecessary reference data re-generation
    Previously, these constants/reference data were re-generated for
    each note in a serialized song, each time a serialized song was
    generated. (I.e., a lot of times during normal usage of the app).
    However, this data never changes so there's no reason to do so.
    
    Although I don't think this had a practical negative impact, it
    seems like an easy thing to avoid.

--- src/synth_core/note.js
+++ src/synth_core/note.js
@@ -2,75 +2,75 @@
 
 var NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 var NOTES_IN_OCTAVE = 12;
+var MIDDLE_OCTAVE = 4;
+var MIDDLE_A_FREQUENCY = 440.0;
+
+var NOTE_RATIOS = {
+  "A"  : 1.0,
+  "A#" : Math.pow(2,  1 / 12),
+  "B"  : Math.pow(2,  2 / 12),
+  "C"  : Math.pow(2,  3 / 12),
+  "C#" : Math.pow(2,  4 / 12),
+  "D"  : Math.pow(2,  5 / 12),
+  "D#" : Math.pow(2,  6 / 12),
+  "E"  : Math.pow(2,  7 / 12),
+  "F"  : Math.pow(2,  8 / 12),
+  "F#" : Math.pow(2,  9 / 12),
+  "G"  : Math.pow(2, 10 / 12),
+  "G#" : Math.pow(2, 11 / 12),
+};
+
+var ENHARMONIC_EQUIVALENTS = {
+  "A"   : "A",
+  "G##" : "A",
+  "B@@" : "A",
+
+  "A#"  : "A#",
+  "B@"  : "A#",
+  "C@@" : "A#",
+
+  "B"   : "B",
+  "A##" : "B",
+  "C@"  : "B",
+
+  "C"   : "C",
+  "B#"  : "C",
+  "D@@" : "C",
+
+  "C#"  : "C#",
+  "B##" : "C#",
+  "D@"  : "C#",
+
+  "D"   : "D",
+  "C##" : "D",
+  "E@@" : "D",
+
+  "D#"  : "D#",
+  "E@"  : "D#",
+  "F@@" : "D#",
+
+  "E"   : "E",
+  "D##" : "E",
+  "F@"  : "E",
+
+  "F"   : "F",
+  "E#"  : "F",
+  "G@@" : "F",
+
+  "F#"  : "F#",
+  "E##" : "F#",
+  "G@"  : "F#",
+
+  "G"   : "G",
+  "F##" : "G",
+  "A@@" : "G",
+
+  "G#"  : "G#",
+  "A@"  : "G#",
+};
+
 
 export function Note(newNoteName, newOctave, newStepCount) {
-  var NOTE_RATIOS = {
-    "A"  : 1.0,
-    "A#" : Math.pow(2,  1 / 12),
-    "B"  : Math.pow(2,  2 / 12),
-    "C"  : Math.pow(2,  3 / 12),
-    "C#" : Math.pow(2,  4 / 12),
-    "D"  : Math.pow(2,  5 / 12),
-    "D#" : Math.pow(2,  6 / 12),
-    "E"  : Math.pow(2,  7 / 12),
-    "F"  : Math.pow(2,  8 / 12),
-    "F#" : Math.pow(2,  9 / 12),
-    "G"  : Math.pow(2, 10 / 12),
-    "G#" : Math.pow(2, 11 / 12),
-  };
-
-  var ENHARMONIC_EQUIVALENTS = {
-    "A"   : "A",
-    "G##" : "A",
-    "B@@" : "A",
-
-    "A#"  : "A#",
-    "B@"  : "A#",
-    "C@@" : "A#",
-
-    "B"   : "B",
-    "A##" : "B",
-    "C@"  : "B",
-
-    "C"   : "C",
-    "B#"  : "C",
-    "D@@" : "C",
-
-    "C#"  : "C#",
-    "B##" : "C#",
-    "D@"  : "C#",
-
-    "D"   : "D",
-    "C##" : "D",
-    "E@@" : "D",
-
-    "D#"  : "D#",
-    "E@"  : "D#",
-    "F@@" : "D#",
-
-    "E"   : "E",
-    "D##" : "E",
-    "F@"  : "E",
-
-    "F"   : "F",
-    "E#"  : "F",
-    "G@@" : "F",
-
-    "F#"  : "F#",
-    "E##" : "F#",
-    "G@"  : "F#",
-
-    "G"   : "G",
-    "F##" : "G",
-    "A@@" : "G",
-
-    "G#"  : "G#",
-    "A@"  : "G#",
-  };
-
-  var MIDDLE_OCTAVE = 4;
-  var MIDDLE_A_FREQUENCY = 440.0;
-
   var calculateFrequency = function(normalizedNoteName, octave) {
     var octaveExponent = octave - MIDDLE_OCTAVE;
     var octaveMultiplier;
