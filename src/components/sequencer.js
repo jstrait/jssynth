jstrait/jssynth

commit 2618e646a289f7146d730282c385e1f61984f935

Preventing spurious drags when using context menu
Previously, right-clicking on a pattern and then selecting an item
from the browser context menu that is displayed would cause the
pattern to be in a bad drag state - an active drag would be in
progress, even though no mouse buttons were being pressed. It
would also usually result in a bunch of errors in the console.

Now, a drag will only begin when the left mouse button is clicked.
The right mouse button still allows the browser context menu to be
displayed, but won't start a drag.

As an alternative I considered disabling the context menu
altogether so that the right mouse button could be used for
dragging. However, I would prefer to keep default browser behavior
where possible, and dragging with the right mouse button seems
like edge case behavior.

src/components/sequencer.js
@@ -515,9 +515,12 @@ class TimelinePattern extends React.PureComponent {
 
   onMouseDown(e) {
     this.selectSelf();
-    this.props.setIsPopupMenuPending(this.props.patternID);
-    this.props.setIsPopupMenuActive(false);
-    this.props.startDrag(this.props.patternID, this.props.trackIndex, this.props.startStep, this.props.fullStepCount, e.clientX);
+
+    if (e.button === 0) {  // Prevent drag errors when using context menu
+      this.props.setIsPopupMenuPending(this.props.patternID);
+      this.props.setIsPopupMenuActive(false);
+      this.props.startDrag(this.props.patternID, this.props.trackIndex, this.props.startStep, this.props.fullStepCount, e.clientX);
+    }
 
     // Prevent click on parent pattern grid container
     e.stopPropagation();
@@ -553,7 +556,9 @@ class TimelinePattern extends React.PureComponent {
       this.props.setIsPopupMenuActive(false);
     }
 
-    this.props.startResize(this.props.startStep);
+    if (e.button === 0) { // Prevent drag errors when using context menu
+      this.props.startResize(this.props.startStep);
+    }
 
     // Prevent a drag start occuring on parent
     e.stopPropagation();
@@ -565,7 +570,9 @@ class TimelinePattern extends React.PureComponent {
       this.props.setIsPopupMenuActive(false);
     }
 
-    this.props.startLoopChange(this.props.startStep, this.props.baseStepCount);
+    if (e.button === 0) { // Prevent drag errors when using context menu
+      this.props.startLoopChange(this.props.startStep, this.props.baseStepCount);
+    }
 
     // Prevent a drag start occuring on parent
     e.stopPropagation();

commit 7bd8fbd997076cd5a0e3378c4386d4fb822b937c

Fixing resize/loop pattern buttons for touch events
2618e646a289f7146d730282c385e1f61984f935 accidentally broke the
"resize" and "loop" pattern buttons when using touch events, such
that touching them did nothing.

That commit added a change to make the primary mouse button the
only mouse button that could be used with the "resize" and "loop"
pattern buttons. However, the check for which mouse button was
being pressed assumed that all events have a `button` property,
but this property is undefined for touch events. This caused touch
events to be ignored.

src/components/sequencer.js
@@ -603,7 +603,7 @@ class TimelinePattern extends React.PureComponent {
       this.props.setIsPopupMenuActive(false);
     }
 
-    if (e.button === 0) { // Prevent drag errors when using context menu
+    if (e.button !== undefined && e.button === 0) { // Prevent drag errors when using context menu
       this.props.startResize(this.props.startStep);
     }
 
@@ -617,7 +617,7 @@ class TimelinePattern extends React.PureComponent {
       this.props.setIsPopupMenuActive(false);
     }
 
-    if (e.button === 0) { // Prevent drag errors when using context menu
+    if (e.button !== undefined && e.button === 0) { // Prevent drag errors when using context menu
       this.props.startLoopChange(this.props.startStep, this.props.baseStepCount);
     }
 