jstrait/jssynth

commit 59da53e849c547c7353232df919a986ef28b82b6

Upgrading React v17.0.2 -> v18.2.0
The `flushSync()` calls are needed to work around React 18's new
automatic batching of state updates. Without them, some logic that
uses the current component state will not see the correct up to
date state.

The biggest impact of the `flushSync()` calls not being present is
that upon app initialization the default song won't be properly
synced to the synth layer, leading to silence during song playback
and errors when interacting with the app. Piano keyboards will
also be affected, going into a bad state when dragging from a
piano key onto one of the scroll buttons.

src/app.js
@@ -1,7 +1,8 @@
 "use strict";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { flushSync } from "react-dom";
 
 import * as SynthCore from "./synth_core";
 import * as DefaultSong from "./default_song";
@@ -182,17 +183,19 @@ class App extends React.Component {
         let channelID;
         let instrument;
 
-        this.setState((prevState, props) => ({
-          isLoaded: true,
-          measureCount: DefaultSong.measureCount,
-          transport: Object.assign({}, prevState.transport, {
-            tempo: DefaultSong.tempo,
-          }),
-          instruments: DefaultSong.instruments,
-          patterns: DefaultSong.patterns,
-          tracks: DefaultSong.tracks,
-          isMidiEnabled: this.midiController.isEnabled(),
-        }));
+        flushSync(() => {
+          this.setState((prevState, props) => ({
+            isLoaded: true,
+            measureCount: DefaultSong.measureCount,
+            transport: Object.assign({}, prevState.transport, {
+              tempo: DefaultSong.tempo,
+            }),
+            instruments: DefaultSong.instruments,
+            patterns: DefaultSong.patterns,
+            tracks: DefaultSong.tracks,
+            isMidiEnabled: this.midiController.isEnabled(),
+          }));
+        });
 
         this.transport.setTempo(this.state.transport.tempo);
 
@@ -1330,4 +1333,4 @@ class App extends React.Component {
   };
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(<App />);

src/components/keyboard.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import React from "react";
+import { flushSync } from "react-dom";
 
 const NOTE_NAMES = ["A", "B", "C", "D", "E", "F", "G"];
 
@@ -91,9 +92,11 @@ class Keyboard extends React.PureComponent {
     }
 
     if (newScrollLeftTimeoutID !== this.state.scrollLeftTimeoutID || newScrollRightTimeoutID !== this.state.scrollRightTimeoutID) {
-      this.setState({
-        scrollLeftTimeoutID: newScrollLeftTimeoutID,
-        scrollRightTimeoutID: newScrollRightTimeoutID,
+      flushSync(() => {
+        this.setState({
+          scrollLeftTimeoutID: newScrollLeftTimeoutID,
+          scrollRightTimeoutID: newScrollRightTimeoutID,
+        });
       });
     }
 
@@ -218,7 +221,7 @@ class Keyboard extends React.PureComponent {
 
     // This event handler is added manually to the actual DOM element, instead of using the
     // normal React way of attaching events because React seems to have a bug that prevents
-    // preventDefault() from working correctly in a "touchmove" handler (as of v17.0.2).
+    // preventDefault() from working correctly in a "touchmove" handler (as of v18.2.0).
     // The preventDefault() is needed to prevent the "pinch zoom into page" gesture from
     // activating when using the keyboard on iOS.
     // See https://medium.com/@ericclemmons/react-event-preventdefault-78c28c950e46 and

src/components/sequencer.js
@@ -452,7 +452,7 @@ class TimelineGrid extends React.Component {
   componentDidMount() {
     // This event handler is added manually to the actual DOM element, instead of using the
     // normal React way of attaching events because React seems to have a bug that prevents
-    // preventDefault() from working correctly in a "touchmove" handler (as of v17.0.2).
+    // preventDefault() from working correctly in a "touchmove" handler (as of v18.2.0).
     // The preventDefault() is needed to prevent the container element from scrolling while
     // a touch drag is active on iOS.
     // See https://medium.com/@ericclemmons/react-event-preventdefault-78c28c950e46 and

commit d6f610b0bcdd4145812479cbd0fd19d08cd37952

Fixing erroneous piano key re-triggering
The React upgrade in 59da53e849c547c7353232df919a986ef28b82b6
(v17 -> v18) introduced a bug that wasn't caught in that commit.
After triggering a piano keyboard key with the mouse, dragging off
the key with the mouse button down, releasing the mouse button,
and then moving the mouse cursor back onto the keyboard with the
mouse button released, the piano key (or scroll button) under the
cursor would be erroneously re-triggered.

The problem was a missing `flushSync()` call, needed when using
React v18. Without it, a state update in a `mouseover` event to
set the piano keyboard to "deactivated" state will not be visible
to an immediately subsequent `mousemove` event, causing the issue.

src/app.js
@@ -957,8 +957,12 @@ class App extends React.Component {
   };
 
   deactivateKeyboard() {
-    this.setState({
-      isKeyboardActive: false
+    // `flushSync()` is needed to make sure mouse events that happen immediately
+    // afterward see the state update.
+    flushSync(() => {
+      this.setState({
+        isKeyboardActive: false
+      });
     });
   };
 